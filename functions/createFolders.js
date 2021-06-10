7ea610b996b3263b6226136c34cb4cee44d2f138
Prof01 <masubreel@gmail.com>
2021-06-10 05:23:29 +0000

    make it better

--- functions/createFolders.js
+++ functions/createFolders.js
@@ -54,9 +54,9 @@ async function createFolders(res, dictsName, distCode, totalSAs) {
 
     // Create Zip File of Folders created
     await zip(`./storage/folders/${distCode}`, `./storage/zipfiles/${distCode}/${districtName} BACKUP_FOLDER.zip`)
-    .then(() => res.sendFile(path.join(__dirname, `../storage/zipfiles/${distCode}/${districtName} BACKUP_FOLDER.zip`)))
-    .catch(err => res.status(500).send({msg: err}))
-    // .catch(err => res.status(500).send('<h2>Oops Request Timeout</h2>'))
+    .then(async () => await res.sendFile(path.join(__dirname, `../storage/zipfiles/${distCode}/${districtName} BACKUP_FOLDER.zip`)))
+    // .catch(err => res.status(500).send({msg: err}))
+    .catch(err => res.status(500).send('<h2>Oops Request Timeout. Retry Again</h2>'))
 
 }
 
